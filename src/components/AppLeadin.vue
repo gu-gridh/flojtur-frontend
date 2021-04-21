bfb176b9557dbfff067f2ab95ee1cb63ab7571e8
Arild Matsson <arild.matsson@gu.se>
2021-04-21 18:14:02 +0200

    Responsive app leadin

--- src/components/AppLeadin.vue
+++ src/components/AppLeadin.vue
@@ -1,22 +1,22 @@
 <template>
-  <section class="app-leadin">
-    <div id="ItemLeft">
-      <div class="articleIngress">
+  <section class="app-leadin container">
+    <div class="first">
+      <p>
         Pehr Strand (c. 1758–1826) var orgel- och flöjtursbyggare i Stockholm
         kring sekelskiftet 1800. Denna webbportal är en redovisning av ett
         forskningsprojekt och samtidigt en källa till information om Pehr Strand
         och i synnerhet hans flöjtur.
-      </div>
+      </p>
     </div>
-    <div id="ItemRight">
-      <div class="articleIngress">
+    <div class="second">
+      <p>
         Flöjtur är en klocka med ett mekaniskt spelverk som kan spela
         musikstycken programmerade på en stiftvals. Termen flöjtur anger också
         att tonen alstras med hjälp av pipor på samma sätt som i traditionella
         piporglar. I Sverige byggdes denna typ av flöjtur främst under andra
         hälften av 1700-talet och en bit in på 1800-talet, där Pehr Strand var
         en av de mest framgångsrika byggarna.
-      </div>
+      </p>
     </div>
   </section>
 </template>
@@ -27,40 +27,47 @@ export default {};
 
 <style lang="scss" scoped>
 .app-leadin {
-  width: 80%;
   height: auto;
-  margin: 50px auto 0px;
-  overflow: hidden;
-}
-
-#ItemLeft {
-  width: 47.5%;
-  height: auto;
-  padding: 0px 30px 0px 0px;
-  float: left;
-  border-style: solid;
-  border-width: 0px 0.5px 0px 0px;
-}
-#ItemRight {
-  width: 45.5%;
-  height: auto;
-  margin-top: 10px;
-  margin-left: 30px;
-  float: left;
-}
-
-.articleIngress {
-  columns: 1;
+  margin-top: 50px;
   line-height: 1.2;
+  text-align: center;
+}
 
-  #ItemLeft & {
-    font-size: 30px;
+.first {
+  font-size: 30px;
+}
+
+.second {
+  font-size: 22px;
+}
+
+@media screen and (min-width: 1000px) {
+  .app-leadin {
+    display: flex;
+    align-items: center;
+  }
+
+  .first {
+    flex: 1;
+    padding-right: 30px;
+    border-right: 0.5px solid;
     text-align: right;
   }
 
-  #ItemRight & {
+  .second {
+    flex: 1;
+    margin-left: 30px;
     text-align: left;
-    font-size: 22px;
+  }
+
+  .first,
+  .second {
+    & > :first-child {
+      margin-top: 0;
+    }
+    & > :last-child {
+      margin-bottom: 0;
+    }
   }
 }
 </style>
