70b57f5d012ed3f81f2dd13cddd42cf8bcdde8d8
Arild Matsson <arild.matsson@gu.se>
2021-04-23 19:19:04 +0200

    Responsive extended metadata close button

--- src/components/ShowMore.vue
+++ src/components/ShowMore.vue
@@ -43,16 +43,33 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .deactivateBonusMaterial {
   float: left;
   margin-left: -77px;
   background: url(/interface/close.png);
-  background-size: cover;
+  background-size: contain;
   width: 40px;
   height: 40px;
   border-radius: 10px;
   cursor: pointer;
+
+  @media screen and (max-width: 1000px) {
+    margin-left: -65px;
+  }
+
+  @media screen and (max-width: 800px) {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    height: 25px;
+    width: 25px;
+    margin-left: 0;
+    background-image: url(../assets/close-white.png);
+    .container > & {
+      right: calc(5% + 5px);
+    }
+  }
 }
 
 .enclosure {
