cc965ae73ca734522997feef44cccab0d059ba32
Arild Matsson <arild.matsson@gu.se>
2021-09-20 11:48:55 +0200

    Footer max-width

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,35 @@
 <template>
   <footer id="footer">
-      <div id="SuperContainer">  
-    <div class="container" style="width:250px; height:100%; ">  
-    <router-link to="/pehr-strand" tag="div" class="IntroInfoContainer">
-            <div id="IntroInfoButton"></div>
-            <div id="IntroInfoLabel">Om Pehr Strand</div>
-          </router-link>
-          <router-link to="/om" tag="div" class="IntroInfoContainer">
-            <div id="IntroInfoButton"></div>
-            <div id="IntroInfoLabel">Om forskningsprojektet</div>
-          </router-link>
-    </div>
-
-    <div class="CitateContainer" style="width:400px;   padding-top: 0.3rem;">
-   
-      <div> Norrback, J. (2021). <em>Pehr Strands spelur</em>. Centrum för digital
-        humaniora, Göteborgs universitet. https://strand.dh.gu.se/  (version 1).</div>
-
-  
-        <div style="padding-top:0.8rem;">
-          <div></div>
-       <div> Licens: <a href="https://creativecommons.org/licenses/by-nc/4.0/">CC BY-NC 4.0</a>, om inget annat anges.</div>
-       
+    <div id="SuperContainer">
+      <div class="container" style="width: 250px; height: 100%">
+        <router-link to="/pehr-strand" tag="div" class="IntroInfoContainer">
+          <div id="IntroInfoButton"></div>
+          <div id="IntroInfoLabel">Om Pehr Strand</div>
+        </router-link>
+        <router-link to="/om" tag="div" class="IntroInfoContainer">
+          <div id="IntroInfoButton"></div>
+          <div id="IntroInfoLabel">Om forskningsprojektet</div>
+        </router-link>
       </div>
-     </div>
+
+      <div class="CitateContainer">
+        <div>
+          Norrback, J. (2021). <em>Pehr Strands spelur</em>. Centrum för digital
+          humaniora, Göteborgs universitet. https://strand.dh.gu.se/ (version
+          1).
+        </div>
+
+        <div style="padding-top: 0.8rem">
+          <div></div>
+          <div>
+            Licens:
+            <a href="https://creativecommons.org/licenses/by-nc/4.0/"
+              >CC BY-NC 4.0</a
+            >, om inget annat anges.
+          </div>
+        </div>
+      </div>
     </div>
-  
   </footer>
 </template>
 
@@ -35,46 +39,43 @@ export default {};
 
 <style lang="scss" scoped>
 #footer {
-      float:left;
+  float: left;
   background-color: #222;
   color: white;
   line-height: 1.3;
-  height:auto;
-  width:100%;
+  height: auto;
+  width: 100%;
   font-size: 18px;
 }
-#SuperContainer{
-    float:left;
-padding-top:1.5rem;
-padding-bottom:1.5rem;
- 
+#SuperContainer {
+  float: left;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
 }
 
- .container {
-  float:left;
-  margin-left:50px;
-
+.container {
+  float: left;
+  margin-left: 50px;
 }
 
- .CitateContainer {
-  float:left;
-  margin-left:50px;
-  border-style:solid; 
-  border-width:0 0px 0 0.5px; 
-  border-color:white;
-  padding:0px 0px 0px 50px;
+.CitateContainer {
+  float: left;
+  max-width: 400px;
+  margin-left: 50px;
+  border-style: solid;
+  border-width: 0 0px 0 0.5px;
+  border-color: white;
+  padding: 0.3rem 0px 0px 50px;
 }
 
 @media screen and (max-width: 800px) {
-    .CitateContainer {
-
-margin-top:20px;
-  border-style:solid; 
-  border-width:0 0px 0 0; 
-  border-color:white;
-    padding:0px 0px 0px 0px;
-}
-
+  .CitateContainer {
+    margin-top: 20px;
+    border-style: solid;
+    border-width: 0 0px 0 0;
+    border-color: white;
+    padding: 0px 0px 0px 0px;
+  }
 }
 
 .IntroInfoContainer {
