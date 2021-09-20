6f2b94e79fcf05dd2fb27f44c4ca2f4360cf2658
Arild Matsson <arild.matsson@gu.se>
2021-09-20 12:42:29 +0200

    New InstrumentAbout text, images

--- src/views/InstrumentAbout.vue
+++ src/views/InstrumentAbout.vue
@@ -7,12 +7,14 @@
       <h1>Ett självspelande flöjtur</h1>
       <div class="articleIngress">
         <p>
-          Ett <strong>flöjtur</strong> är ett golvur som har ett inbyggt
-          mekaniskt spelverk, där tonen alstras med hjälp av pipor liknande dem
-          man finner i traditionella piporglar. I andra typer av spelur alstras
-          ljudet av ett stränginstrument eller av klangskålar. Spelur är en term
-          som syftar på dessa tre grundtyper. I Berlin, med flera mycket
-          skickliga spelurssbyggare, kallades de olika typerna för
+          Ett flöjtur är ett golvur som har ett inbyggt mekaniskt spelverk, där
+          tonen alstras med hjälp av pipor liknande dem man finner i
+          traditionella piporglar. I andra typer av spelur alstras ljudet av ett
+          stränginstrument eller av klangskålar (klockor). Spelur är en term som
+          kan syfta på alla dessa tre grundtyper, medan flöjtur är en specifik
+          term för ett spelur där ljudet alstras av flöjter eller orgelpipor. I
+          slutet av 1700-talet i Berlin, med flera mycket skickliga
+          spelurssbyggare, kallades de olika typerna för
           <em>Flötenuhr</em>, <em>Harfenuhr</em> och <em>Glockenspiel</em>.
         </p>
       </div>
@@ -32,18 +34,21 @@
           grammofoner.” (<em>Grove Music Dictionary</em>).
         </blockquote>
         <p>
-          En kortare definition finner vi i <em>Musik-Lexikon</em>, utgivet av
-          J. Leonard Höijer (1864). Höijer skriver under spelur: ”Klocka, inredd
-          med ett spelverk på samma sätt som en speldosa, och som vid fulla
-          timmar afspelar något musikstycke.”
+          En kortare definition finner vi i ett svenskt <em>Musik-Lexikon</em>,
+          utgivet av J. Leonard Höijer (1864). Höijer skriver under spelur:
+          ”Klocka, inredd med ett spelverk på samma sätt som en speldosa, och
+          som vid fulla timmar afspelar något musikstycke.” Kombinationen av
+          klockor och spelverk speglar 1700-talets mekanistiska förståelse av
+          världen, med rötterna i upplysningens vetenskapliga landvinningar.
         </p>
         <p>
-          Det viktiga här är det repeterbara programmet. De består vanligen av
-          stift och klamrar av metall som fästs på en trä- eller
-          mässingscylinder. En så kallad <strong>stiftvals</strong>. Det är
-          alltså inte en inspelning, som på en vax- eller vinylskiva, utan ett
-          program som ett mekaniskt verk kan spela och upprepa. I golvuren drivs
-          spelverket av ett blylod som kan väga hela 40 kg.
+          Det viktiga i självspelande instrument är det repeterbara programmet
+          som innehåller musiken som skall spelas. På en musikvals består det
+          vanligen av stift och klamrar av metall som fästs på en trä- eller
+          mässingscylinder. En så kallad stiftvals. Det är alltså inte en
+          inspelning, som på en vinyl- eller CD-skiva, utan ett program som ett
+          mekaniskt verk kan spela och upprepa. I golvuren drivs spelverket av
+          ett blylod som kan väga hela 40 kg.
         </p>
         <p>
           De första exemplen på automatiska instrument finner vi i grekiska
@@ -57,17 +62,139 @@
           Vinci en spinett och två trummor troligen spelande genom en
           programmerad cylinder. Från 1500-talet byggdes spinetter som kunde
           spelas både manuellt eller automatiskt med ett verk och en
-          programmerad cylinder, bäst kända är verken från Augsburg byggda av
-          Bidermann. Urmakeriet genomgick en stark utveckling under slutet av
-          1600-talet, och med den ökade precisionen i gången på klockorna följde
-          även en utveckling av olika självspelande instrument. Här växte bland
-          annat Berlin fram som ett nav för högkvalitativt urmakeri i
-          kombination med mekaniska instrument. Det svenska spelursmakeriet
-          delar flera karaktäristiska drag med det Berlinska.
+          programmerad cylinder, bäst kända är verken från Augsburg i början av
+          1600-talet byggda av familjen Bidermann. Urmakeriet genomgick en stark
+          utveckling under slutet av 1600-talet, och med den ökade precisionen i
+          gången på klockorna följde även en utveckling av olika självspelande
+          instrument. Här växte bland annat Berlin fram som ett nav för
+          högkvalitativt urmakeri i kombination med mekaniska instrument. Det
+          svenska spelursmakeriet delar flera karaktäristiska drag med det
+          Berlinska.
         </p>
-        <h3>Att bygga ett flöjtur [kommer senare]</h3>
-        <h3>Att programmera en stiftvals [kommer senare]</h3>
-        <h3>En optisk valsläsare [kommer senare]</h3>
+
+        <figure>
+          <figcaption>
+            Flöjturet på bilden är 628 mm från botten av bälgstolen till toppen
+            av den längsta pipan. Pallbrädan som spelmekaniken och orgeln vilar
+            på är 480 mm bred och 390 mm djup.
+          </figcaption>
+          <img
+            src="@/assets/article/IllustrationTotal_1001_4kx4k.png"
+            alt="Flöjtur"
+          />
+        </figure>
+        <p>
+          Det finns tre viktiga delar i ett självspelande flöjtur: motorn, eller
+          det mekaniska spelverket; musiken, programmerad på en trävals; orgeln,
+          själva instrumentet som ljuder.
+        </p>
+        <p>
+          Motorn: kraften att driva hela konstruktionen kommer från ett lod,
+          oftast i bly, med en vikt uppemot 40kg. Lodet hänger i en lodlina som
+          är lindad runt en lite träcylinder. Belastningen på lager är stor men
+          samtidigt är precisionen lika viktig och en korrekt uträkningen av
+          förhållandena mellan alla kugghjul är avgörande. När man startar
+          speluret sänks läsarmarna ner samtidigt som bälgen fylls med luft med
+          hjälp av två pumparmar. Verket driver en ihålig trävals med
+          mässingsstift, och för varje varv skall trävalsen förskjutas 2 mm i
+          sidled. Man får på så sätt ett spår som går i en spiral runt
+          trävalsens yta. Musiken som mässingsstiften representerar kan då vara
+          uppemot 3 minuter lång, betydligt längre än på de mindre vevpositiven.
+          När valsen roterat sex varv skall läsarmarna lyftas upp i säkrat läge
+          så att stiftvalsen och musiken kan bytas utan att läsarmarnas stift
+          fastnar i mässingsstiften på trävalsen.
+        </p>
+        <figure>
+          <div class="figure-twopart">
+            <img
+              src="@/assets/article/IllustrationClose_10012_4kx4k.png"
+              alt="Flöjtur"
+            />
+            <img
+              src="@/assets/article/IllustrationClose_10022_4kx4k.png"
+              alt="Flöjtur"
+            />
+          </div>
+          <figcaption>
+            Mellan de två verkplåtarna av mässing ligger motorn med kugghjul av
+            olika storlek i bestämda proportioner. Illustration: Olof Pipping.
+          </figcaption>
+        </figure>
+        <p>
+          Stiftvalsen: på trävalsen programmeras musik genom mässingsstift av
+          olika längd; från vassa stift för mycket korta toner, till långa
+          byglar, för ett långt slutackord. Grundprincipen är gammal, och går
+          tillbaka ända till antikens Alexandria. Det äldsta bevarade exemplet
+          på automatiska musikinstrument är det medeltida klockspelet i
+          katedralen i Beauvais, norr om Paris, från tidigt 1300-tal. Musiken
+          som finns på stiftvalsarna i Strands spelur är musik som spelades
+          t.ex. på operan i Stockholm. Musiken behövde anpassas för den lilla
+          orgeln, så det behövdes en musikaliskt skolad person att arrangera om
+          musiken så den fungerade i speluret. Sedan måste varje enskilt stift
+          markeras och för hand tryckas in i trävalsen. Även här är precisionen
+          viktigt: stiftet måste placeras vinkelrätt mot träytan och tryckas in
+          så längden blev korrekt för att läsarmarna skulle kunna läsa av
+          informationen. När så ett stift trycker upp läsarmen så trycker den
+          ner en pinne på en spelventil i den lilla orgeln, och en ton ljuder.
+        </p>
+
+        <figure>
+          <div class="figure-twopart">
+            <img
+              src="@/assets/article/IllustrationClose_2001_4kx4k.png"
+              alt="Flöjtur"
+            />
+            <img
+              src="@/assets/article/IllustrationClose_2002_4kx4k.png"
+              alt="Flöjtur"
+            />
+          </div>
+          <figcaption>
+            Stiftvalsen av trä innehåller ett musikstycke, och vanligtvis finns
+            det flera valsar som kan bytas ut. Läsarmarna påverkas av stiften på
+            trävalsen och trycker ner ventilen i den lilla orgeln. Illustration:
+            Olof Pipping.
+          </figcaption>
+        </figure>
+        <p>
+          Orgeln: musikinstrumentet i alla Strands bevarade spelur är en liten
+          orgel, vanligen med 31 toner. I vissa fall med två rader av stämmor,
+          och då skapas en möjlighet att registrera om i musiken, det vill säga
+          att man kan variera klangen. Då tillkommer ytterligare två läsarmar
+          som aktiverar eller stänger av den andra stämman – sammanlagt 33
+          läsarmar. Grundstämman är alltid en täckt orgelstämma (Gedakt) och den
+          andra är en stämma med en lite annorlunda klang, ofta en öppen flöjt,
+          och i några få fall en metallstämma (Traversflöjt). Den andra stämman
+          kompletterar grundstämman och ger en färgrikare klang med fler
+          övertoner. Den dynamiska skillnaden är marginell.
+        </p>
+        <figure>
+          <div class="figure-twopart">
+            <img
+              src="@/assets/article/IllustrationClose_3001_4kx4k.png"
+              alt="Flöjtur"
+            />
+            <img
+              src="@/assets/article/IllustrationClose_3002_4kx4k.png"
+              alt="Flöjtur"
+            />
+          </div>
+          <figcaption>
+            Tryckpinnarna går ner i ventilkistan bakom spåntluckan, och öppnar
+            ventilen så luften kan komma till orgelpipan. Illustration: Olof
+            Pipping.
+          </figcaption>
+        </figure>
+        <p style="text-align: center">* * *</p>
+        <p>
+          Hela 3D-modellen kan laddas ner här (Licens: CC BY-NC 4.0.
+          Dokumentation och CAD-ritning av Olof Pipping.): [Lägg till stp-filen]
+        </p>
+        <p>
+          Här finns en dokumentärfilm om rekonstruktionen av spelverket och
+          piporna till Kungl. Vitterhetsakademiens flöjtur.
+          [https://youtu.be/FIbxJ5GKR0M]
+        </p>
       </div>
     </article>
   </div>
@@ -81,5 +208,14 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.figure-twopart {
+  display: flex;
+  max-width: 100%;
+
+  img {
+    max-width: 50%;
+    flex: 1;
+  }
+}
 </style>
