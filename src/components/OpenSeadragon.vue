6f481e88ba5e1a7d4019ce952b7e2b6c02653f8b
Arild Matsson <arild.matsson@gu.se>
2021-09-22 11:39:30 +0200

    Extract OpenSeadragon component

---
+++ src/components/OpenSeadragon.vue
@@ -0,0 +1,57 @@
+<template>
+  <div ref="osd" class="osd"></div>
+</template>
+
+<script>
+import OpenSeadragon from "openseadragon";
+
+export default {
+  name: "OpenSeadragon",
+  props: {
+    tileSources: Array,
+    cover: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data: () => ({
+    viewer: null,
+  }),
+  mounted() {
+    this.viewer = OpenSeadragon({
+      /** @see https://openseadragon.github.io/docs/OpenSeadragon.html#.Options */
+      element: this.$refs.osd,
+      homeFillsViewer: this.cover,
+      immediateRender: true,
+      visibilityRatio: this.cover ? 1 : 0.5,
+      minZoomImageRatio: this.cover ? 1 : 0.5,
+      showZoomControl: false,
+      showHomeControl: false,
+      showFullPageControl: !!this.filename,
+      prefixUrl: "/openseadragon/",
+      tileSources: this.tileSources,
+    });
+
+    // Zoom in a bit over the middle.
+    if (this.cover) {
+      // Thanks https://github.com/openseadragon/openseadragon/issues/979
+      this.viewer.addHandler("open", function (event) {
+        // Args are (x, y, width, height) as fractions of full image width/height
+        const newBounds = new OpenSeadragon.Rect(0, 0.3, 1, 0);
+        event.eventSource.viewport.fitBoundsWithConstraints(newBounds, true);
+      });
+    }
+  },
+  watch: {
+    tileSources() {
+      this.viewer.open(this.tileSources);
+    },
+  },
+};
+</script>
+
+<style>
+.osd {
+  height: 70vh;
+}
+</style>
--- src/components/ZoomViewer.vue
+++ src/components/ZoomViewer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div ref="osd" class="osd"></div>
+  <OpenSeadragon :tileSources="[tileSource()]" :cover="cover" />
 </template>
 
 <script>
-import OpenSeadragon from "openseadragon";
+import OpenSeadragon from "@/components/OpenSeadragon.vue";
 
 export default {
   name: "ZoomViewer",
@@ -14,34 +14,7 @@ export default {
       default: true,
     },
   },
-  data: () => ({
-    viewer: null,
-  }),
-  mounted() {
-    this.viewer = OpenSeadragon({
-      /** @see https://openseadragon.github.io/docs/OpenSeadragon.html#.Options */
-      element: this.$refs.osd,
-      homeFillsViewer: this.cover,
-      immediateRender: true,
-      visibilityRatio: this.cover ? 1 : 0.5,
-      minZoomImageRatio: this.cover ? 1 : 0.5,
-      showZoomControl: false,
-      showHomeControl: false,
-      showFullPageControl: !!this.filename,
-      prefixUrl: "/openseadragon/",
-      tileSources: [this.tileSource()],
-    });
-
-    // Zoom in a bit over the middle.
-    if (this.cover) {
-      // Thanks https://github.com/openseadragon/openseadragon/issues/979
-      this.viewer.addHandler("open", function (event) {
-        // Args are (x, y, width, height) as fractions of full image width/height
-        const newBounds = new OpenSeadragon.Rect(0, 0.3, 1, 0);
-        event.eventSource.viewport.fitBoundsWithConstraints(newBounds, true);
-      });
-    }
-  },
+  components: { OpenSeadragon },
   methods: {
     tileSource() {
       // Default to blurry placeholder.
@@ -56,11 +29,6 @@ export default {
             type: "image",
             url: `https://data.dh.gu.se/flojtur/${this.filename}`,
           };
-    },
-  },
-  watch: {
-    filename() {
-      this.viewer.open(this.tileSource());
     },
   },
 };
