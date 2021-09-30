8f9f7701dfd13a74c4cb98654be10e7263f5f931
Arild Matsson <arild.matsson@gu.se>
2021-09-30 11:38:51 +0200

    Add missing Ratio.vue

---
+++ src/components/Ratio.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ratio-outer" :style="{ paddingTop: 100 / Number(ratio) + '%' }">
+    <div class="ratio-inner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ratio",
+  props: ["ratio"],
+};
+</script>
+
+<style scoped>
+.ratio-outer {
+  width: 100%;
+  /* All paddings are relative to parent width. */
+  /* Ratio by default, set ratio="" to override. */
+  padding-top: 100%;
+  position: relative;
+}
+.ratio-inner {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+</style>
