e379de8b3c21677957d283754207a9f41492679e
TristanBridge <tbridge360@gmail.com>
2024-04-09 14:56:26 +0200

    matomo install

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ import { VueMasonryPlugin } from "vue-masonry";
 import VueScrollTo from "vue-scrollto";
 import App from "./App.vue";
 import router from "./router";
+import matomo from 'vue-matomo';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueMasonryPlugin);
 Vue.use(VueScrollTo);
+
+if (process.env.VUE_APP_MATOMO_URL && process.env.VUE_APP_MATOMO_SITE_ID) {
+  Vue.use(matomo, {
+    host: process.env.VUE_APP_MATOMO_URL,
+    siteId: process.env.VUE_APP_MATOMO_SITE_ID,
+    router: router,
+    enableLinkTracking: true,
+    trackInitialView: true,
+    debug: false
+  });
+}
 
 new Vue({
   router,
